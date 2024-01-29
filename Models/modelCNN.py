9eddb75f00bfda4b0d7f6e4200ef087ef15a34b6
Mohammad Sayed Zaky <93533860+MohammadSayed02@users.noreply.github.com>
2024-01-29 17:25:43 +0200

    commit

---
+++ Models/modelCNN.py
@@ -0,0 +1,25 @@
+# Define the model
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, random_split
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
+
+
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super(SimpleCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(32 * 64 * 64, 256)
+        self.fc2 = nn.Linear(256, 1)
+
+    def forward(self, x):
+        x = self.pool(nn.functional.relu(self.conv1(x)))
+        x = self.pool(nn.functional.relu(self.conv2(x)))
+        x = x.view(-1, 32 * 64 * 64)
+        x = nn.functional.relu(self.fc1(x))
+        x = torch.sigmoid(self.fc2(x))
+        return x
